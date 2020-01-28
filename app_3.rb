4d16d9a8331f274ebd5b8e01ca9abde714631133
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-01-28 19:10:43 +0100

    game + app_03

--- app_3.rb
+++ app_3.rb
@@ -3,3 +3,7 @@ Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
+require_relative 'lib/game'
+
+my_game = Game.new("Wolverine")
+binding pry
--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,26 @@
-class Game
-  attr_accessor :human_player :enemies
+require_relative 'player'
 
-  def initialize (human_player, enemies)
-    @human_player = human_player
-    @enemies = 
+class Game
+  attr_accessor :human_player, :enemies
+
+  def initialize(name)
+    @human_player = HumanPlayer.new(name)
+    @enemies = Array.new
+    4.times do |i|
+      @enemies << Player.new("player#{i+1}")
+    end
+    puts @enemies
+  end
+
+  def kill_player(player)
+    @enemies.each do |enemie|
+      if enemie.name == player
+        enemies.delete_at(enemie.index)
+      end
+    end
+  end
+
+
+end
+
+
